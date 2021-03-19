da1ad4b185c26ee9ea120c364d7c9b73fc5a429b
Hilary Nelson <hinelson@students.pitzer.edu>
2021-03-18 20:37:56 -0700

    added README

---
+++ README.md
@@ -0,0 +1,11 @@
+
+To generate a PDF version sheet music from the info generated from the semantic model:
+    
+1. Download LilyPond (http://lilypond.org/index.html)
+
+2. Copy the output of the semantic model into plain text (you can 
+    just paste it into spotlight, a search bar, or enter it the command line to do so)
+
+3. Run the generate_music function with the plain text output
+
+4. Open the generated .ly (LilyPond) file, and compile (command + R)
--- sheet-music-parser-1.py
+++ sheet-music-parser-1.py
@@ -1,11 +1,3 @@
-""" To generate a PDF version sheet music from the info generated from the semantic model:
-    1. Download LilyPond (http://lilypond.org/index.html)
-    2. Copy the output of the semantic model into plain text (you can 
-    just paste it into spotlight or the google search bar to do this)
-    3. Run the generate_music function with the plain text output
-    4. Open the generated .ly (LilyPond) file, and compile (command + R)
-"""
-
 # Test input:
 
 """clef-C1
